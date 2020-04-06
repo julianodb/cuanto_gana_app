11864f86b829fb1f7edc36ae1c4d63b6a54031d0
Juliano Barboza <julianodawid@gmail.com>
2020-04-05 20:52:44 -0400

    replaces index with about component

---
+++ components/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <div class="column is-half is-offset-one-quarter">
+      <p>
+        Cuanto Gana Chile es un buscador de cuanto ganan los funcionarios públicos chilenos.
+        A través de una interfaz simple e intuitiva, apoya a toda persona interesada en explorar los gastos por concepto de remuneración impartidos por el Estado de Chile.
+      </p>
+    </div>
+  </div>
+</template>
