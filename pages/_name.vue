da9604053078314b1a3fbef0fc3b657fa6e936e1
Juliano Barboza <julianodawid@gmail.com>
2020-04-05 22:26:39 -0400

    changes pages/_personid to _names

---
+++ pages/_name.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="column">
+    <h1 class="title">
+      {{ personid }}
+    </h1>
+    <div class="subtitle">
+      {{ personData }}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  async asyncData (context) {
+    const personid = context.params.name
+    const personData = await context.$axios.$get(`https://storage.scrapinghub.com/items/434931/1/20/${personid}`, {
+      auth:
+       {
+         username: context.env.apiKey,
+         password: ''
+       }
+    })
+    return { personid, personData }
+  }
+}
+</script>
